ab375841f149475da30ecbdd0d9c31a3d0bf36c3
Yaroslav Ilin <yi@june.dk>
2018-05-28 19:30:47 +0200

    wireframe the category overview page

--- src/CategoryPage/CategoryPage.tsx
+++ src/CategoryPage/CategoryPage.tsx
@@ -7,17 +7,38 @@ export interface IMatchParams {
   category: string;
 }
 
-export function CategoryPage (props: RouteComponentProps<IMatchParams>) {
-  const { match } = props;
-  const { params } = match;
-  const { category } = params;
+export class CategoryPage extends Component<RouteComponentProps<IMatchParams>> {
+  public render () {
+    const { match } = this.props;
+    const { params } = match;
+    const { category } = params;
 
-  return (
-    <main>
-      <p>{category}</p>
-      <form>
-        <input name='comment' />
-      </form>
-    </main>
-  );
+    const items = [
+      { comment: 'barbershop' },
+    ];
+
+    return (
+      <main>
+        <h1>Category: <span>{category}</span></h1>
+
+        <form onSubmit={this.onCommentSubmit}>
+          <h2>Create New Activity</h2>
+          <input name='comment' placeholder='Details' />
+          <input type='submit' value='Add Activity' />
+        </form>
+
+        <div>
+          <h2>Latest Activities in The Category</h2>
+          <ul>
+            {items.map((item, id) => <li key={id}>{item.comment}</li>)}
+          </ul>
+        </div>
+      </main>
+    );
+  }
+
+  protected onCommentSubmit: FormEventHandler<HTMLFormElement> = (evt) => {
+    evt.preventDefault();
+    console.log(evt.currentTarget.comment.value);
+  }
 }
